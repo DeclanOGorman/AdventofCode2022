96883044fd2853def18f1cd0e22d4bb54068e022
DeclanOGorman <dogorman@outlook.com>
2022-12-18 08:14:12 +0000

    Day 15 Part 2

--- 15/code.py
+++ 15/code.py
@@ -1,15 +1,25 @@
+import shapely
+from shapely.geometry import LineString, Point
 input = [a.split(':') for a in open('./15/input.txt', 'r') ]
 input = [[a[0].replace('Sensor at x=','').split(', y='), a[1].replace('closest beacon is at x=','').split(', y=')] for a in input]
 input = [[tuple(map(int,a[0])), tuple(map(int,a[1]))] for a in input]
+
+def add(a,b) :
+    return (a[0] + b[0], a[1] + b[1])
+
+def mul(a,b) :
+    return (a[0] * b, a[1] * b)
+
+def getHash(loc) :
+    return loc[0]*4000000 + loc[1]
 
 def getDist(a, b) :
     return abs(b[0] - a[0]) + abs(b[1] - a[1])
 
 def isCovered(loc, input) :
     for s in input :
-        if getDist(loc, s[0]) <= s[2] : 
-            return (s[0][0]+s[2] - abs(loc[1] - s[0][1])) - loc[0]
-    return -1
+        if getDist(loc, s[0]) <= s[2] : return True
+    return False
 
 input = [[a[0],a[1],getDist(a[0],a[1])] for a in input]
 minMaxX = (min([min(a[0][0], a[1][0]) for a in input]), max([max(a[0][0], a[1][0]) for a in input]))
@@ -17,31 +27,28 @@ minMaxY = (min([min(a[0][1], a[1][1]) for a in input]), max([max(a[0][1], a[1][1
 noBeacon = 0; beacons = set([a[1] for a in input]); y = 2000000
 for x in range(minMaxX[0]-100000, minMaxX[1]+100000) :
     if (x,y) in beacons : continue
-    if not isCovered((x, y), input) == -1: 
+    if isCovered((x, y), input): 
         noBeacon += 1
 
 print(f'Part A: Number of positions with no beacon {noBeacon}') # assert 26
 
-# PART 2 needs more work, O(n^3) complexity clearly not built for brute force!! :D
-b = (0,0)
-for y in range(minMaxY[0], minMaxY[1]) :
-    found = False
-    x = minMaxX[0]
-    while x <= minMaxX[1] :
-        loc = (x, y)
-        offset = isCovered(loc, input)
-        if not offset == -1 : 
-            found = True
-            x = x + offset
-            continue
-        else :
-            if found == True and isCovered((x+1,y),input) > -1 :
-                b = loc
-                break
-        x = x+1
-    if not b == (0,0): break
+# Hypothesis: the point is outside 4 sensor range edges, so intersects x4 sensor +1 vertices
+vectors = [(0,1),(1,0),(0,-1),(-1,0)]; edges = []; intersect = dict()
+for s, b, d in input : #Build edges +1
+    p = [add(s,mul(a,(d+1))) for a in vectors]
+    edges += [(p[0],p[1]),(p[1],p[2]),(p[2],p[3]),(p[3],p[0])]
 
-print(f'Part B: Decode signal at {loc} is {loc[0]*4000000 + loc[1]}') # (14,11) / 56000011
+for a in edges : #Find edge intersections and count
+    for b in edges :
+        if a == b : continue
+        int_pt = LineString(a).intersection(LineString(b))
 
+        if int_pt.is_empty == False and int_pt.type != 'LineString' :
+            i = (int(int_pt.x), int(int_pt.y))
+            h = getHash(i)
+            if not h in intersect : intersect[h] = []
+            intersect[h].append([a,b,i])
 
-
+for i in intersect : 
+    if len(intersect[i]) == 8 : # because we inspect each line twice 8 vs 4
+        print(f'Part B: Decode signal is {i}') # (14,11) / 56000011
